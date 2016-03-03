7f59e0470a712b9c2e479dc1cfb5e1bc7be13ea7
Qaisar Nadeem <iamqaisarnadeem@gmail.com>
2016-03-03 12:15:22 +0500

    Users registration by loggen in user added

---
+++ db/migrate/20160303064438_add_default_admin_user.rb
@@ -0,0 +1,5 @@
+class AddDefaultAdminUser < ActiveRecord::Migration
+  def change
+    User.create(:email=>'admin@archiscope.com',:password=>'admin123',:first_name=>'Archiscope',:last_name=>'Admin')
+  end
+end
