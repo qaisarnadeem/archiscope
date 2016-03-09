2ce97b0a137e2d31cae7a033a9c0f29d804f2793
Qaisar Nadeem <iamqaisarnadeem@gmail.com>
2016-03-09 12:44:25 +0500

    404 handled for Record not found & no route matching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  rescue_from ActionController::RoutingError, :with =>:render_404
+
+  def render_404
+    render 'main/404' ,:status=>404,:formats=> nil and return
+  end
 end
