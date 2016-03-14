450162549b29d7b852c5fd4680d655c301ac107f
tahirnawaz <m.jam.tahir@gmail.com>
2016-03-14 08:47:37 +0500

    design changes nav

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -1,40 +1,48 @@
     trigger = $('.hamburger');
     overlay = $('.overlay');
+
     function hamburger_cross() {
         trigger.toggleClass('is-open');
         trigger.toggleClass('is-closed');
         overlay.toggle();
     }
-$(document).ready(function() {
+    $(document).ready(function() {
+        /*=======================
+               Scroll
+           =========================*/
+
+
+    });
     /*=======================
-           Scroll
-       =========================*/
+               Change Icon First
+        =========================*/
+    // function close_div() {
+    //     overlay.hide();
+    //     trigger.removeClass('is-closed');
+    //     $('#sidebar-wrapper').hide();
+
+    //     $('#wrapper').removeClass('toggled');
+
+    // }
+
+    function optoin_slide() {
+        $('#show_down').slideToggle('down');
+    }
+
+    function nav_bar() {
+        $('#nav_bar').show();
+        $('#nav_bar').addClass('left_0');
+        $('#page-content-wrapper').addClass('page_left');
+        $('#nav_bar').removeClass('left_219');
+        $(".overlay").css("display", "block");
 
 
-});
-/*=======================
-           Change Icon First
-    =========================*/
-// function close_div() {
-//     overlay.hide();
-//     trigger.removeClass('is-closed');
-//     $('#sidebar-wrapper').hide();
+    }
 
-//     $('#wrapper').removeClass('toggled');
-
-// }
-
-function optoin_slide() {
-    $('#show_down').slideToggle('down');
-}
-
-$(document).ready(function() {
-    trigger = $('.hamburger');
-    trigger.click(function() {
-        hamburger_cross();
-    });
-
-    $('[data-toggle="offcanvas"]').click(function() {
-        $('#wrapper').toggleClass('toggled');
-    });
-});
+    function close_div() {
+        $('#nav_bar').hide();
+        $('#page-content-wrapper').removeClass('page_left');
+        $('#nav_bar').removeClass('left_0');
+        $('#nav_bar').addClass('left_219');
+        $(".overlay").css("display", "none");
+    }
