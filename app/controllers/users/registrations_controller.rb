12d509a8803d1485f2e10aeefffc9757c4bf9c8d
Qaisar Nadeem <iamqaisarnadeem>
2016-03-20 22:17:23 +0500

    auto login at new user issue fixed

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -25,7 +25,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
      if resource.persisted?
        if resource.active_for_authentication?
          #set_flash_message! :notice, :signed_up
-         sign_up(resource_name, resource)
          respond_to do |format|
            format.js
          end
